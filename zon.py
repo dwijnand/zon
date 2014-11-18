e7e8b0655414c7b1703ffb623de58ec400abb57e
Dale Wijnand <dale.wijnand@gmail.com>
2014-11-18 02:18:56 +0000

    Format python.

--- zon.py
+++ zon.py
@@ -2,15 +2,18 @@
 
 import boto.ec2
 
+
 def main():
-  sizes = [ec2_count(region) for region in ['eu-west-1', 'us-east-1', 'us-west-2']]
-  print 'Total instances: ' + str(sum(sizes))
+    sizes = [ec2_count(region) for region in ['eu-west-1', 'us-east-1', 'us-west-2']]
+    print 'Total instances: ' + str(sum(sizes))
+
 
 def ec2_count(region):
-  conn = boto.ec2.connect_to_region(region)
-  reservations = conn.get_all_reservations()
-  instances = [inst for res in reservations for inst in res.instances]
-  return len(instances)
+    conn = boto.ec2.connect_to_region(region)
+    reservations = conn.get_all_reservations()
+    instances = [inst for res in reservations for inst in res.instances]
+    return len(instances)
+
 
 if __name__ == "__main__":
-  main()
+    main()
